af45841696d05c7437b3d68f3e6575694d84676f
Awah Ndingwan <Awahndin@udel.edu>
2015-09-23 00:36:29 -0400

    WE MADE IT

--- src/Tuition.java
+++ src/Tuition.java
@@ -1,31 +1,33 @@
 import java.util.Scanner;
+import org.apache.poi.ss.formula.functions.FinanceLib;
 
 public class Tuition {
-	
 	
 	public static void main(String[] args) {
 		Scanner Input = new Scanner(System.in);
 		System.out.println("initial tuition");
-		int IT = Input.nextInt();
+		double IT = Input.nextDouble();
 		System.out.println("Annual percent increase");
 		float APR = Input.nextFloat();
 		System.out.println("Loan Intrest Rate");
 		float r = Input.nextFloat();
 		System.out.println("months to payback Loan:");
 		float n = Input.nextFloat();	
+		Input.close();
 		
-		//this needs to be turned into a for loop
+
 		double t2, t3, t4, T;
-		t2 = IT*APR;
-		t3 =t2*APR;
-		t4 =t3*APR;
-		T = IT + t2 + t3 +t4;
-		
-		double monthlyPayment = T*(r*(1+r)^n)/((1+r)^n-1);
-		
-		
+		t2 = IT*(APR+1);
+		t3 = t2*(APR+1);
+		t4 =t3*(APR+1);
+		T = IT + t2 + t3 + t4;
 		System.out.println("your full tuition is:" + T);
-		System.out.println("your monthly payment is:" + monthlyPayment);
+		
+		double X= 0;
+		boolean B =false;
+		double PMT = FinanceLib.pmt(r,n,T,X,B);
+		
+		System.out.println("your monthly payment is:" + (-1)*PMT/12);
 	}
 	
 }
